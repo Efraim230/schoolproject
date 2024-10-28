3ce6771792917f631f975baaa414d1516962c2aa
Efraim230 <97708802+Efraim230@users.noreply.github.com>
2024-10-28 17:06:10 +0100

    main.js

--- Js/main.js
+++ Js/main.js
@@ -4,3 +4,19 @@ window.addEventListener("scroll", ()=>
     {
         header.classList.toggle("shadow", window.scrollY > 0)
     })
+
+
+    const toggleBtn =document.querySelector('.lat');
+    const toggleBtnIcon =document.querySelector('.lat i');
+    const dropdown =document.querySelector('.dropdown-menu');
+
+
+    toggleBtn.onclick = function()
+    {
+        dropdown.classList.toggle('open')
+        const isOpen=dropdown.classList.contains('open')
+
+        toggleBtnIcon.classList = isOpen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars'
+    }
